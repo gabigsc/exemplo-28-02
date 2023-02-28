0de2726956a40409e881bceedb0f7d0dc6968815
gabigsc <gabrielagsc2005@gmail.com>
2023-02-28 10:01:20 -0300

    first commit

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML!>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+</head>
+<body>
+     <h1> Acesso do Teste Concluído!!! </h1>
+</body>
+</html>
+
